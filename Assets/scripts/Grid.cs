23d361181dffd51fa03a6c0e09bc35be604db367
Ignacio Villalobos Salas <nachovs91@outlook.es>
2016-07-13 22:35:35 +0200

    A custom Grid generate procedurally

---
+++ Assets/scripts/Grid.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
+public class Grid : MonoBehaviour {
+
+    public int xSize, ySize;
+    private Vector3[] vertices;
+
+    private Mesh mesh;
+
+    private void Generate() {
+
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
+        //calculate number of vertices
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+
+        Vector2[] uv = new Vector2[vertices.Length];
+
+        //positioning vertices and providing Uv coordinates
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+                uv[i] = new Vector2((float)x / xSize,(float) y / ySize);
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+        }
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+
+
+
+    }
+
+
+    private void Awake() {
+        Generate();
+    }
+
+    
+
+    
+   
+
+
+}
